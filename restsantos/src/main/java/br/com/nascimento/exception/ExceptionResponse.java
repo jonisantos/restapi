c820ef2b9d692d3ddd793c25660ca9386e031f27
Jonilson Nascimento <jonnyzerobravo@gmail.com>
2025-06-28 20:43:53 -0400

    Creating Exceptions

---
+++ restsantos/src/main/java/br/com/nascimento/exception/ExceptionResponse.java
@@ -0,0 +1,7 @@
+package br.com.nascimento.exception;
+
+
+
+import java.util.Date;
+
+public record ExceptionResponse(Date timestamp, String message, String details) {}
---
+++ restsantos/src/main/java/br/com/nascimento/exception/UnsupportedMathOperationException.java
@@ -0,0 +1,16 @@
+package br.com.nascimento.exception;
+
+
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsupportedMathOperationException extends RuntimeException {
+
+    public UnsupportedMathOperationException(String message) {
+        super(message);
+    }
+}
